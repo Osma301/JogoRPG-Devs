bbe5c552c8c0b3412e195cce1f5e7055671bcce0
Bruno Wilke <brunowilke153@gmail.com>
2022-12-18 19:57:07 -0300

    incluido receberDano

--- src/DestructorSanguinario.java
+++ src/DestructorSanguinario.java
@@ -1,9 +1,14 @@
 
 
 public class DestructorSanguinario extends Boss {
+	
 	public DestructorSanguinario() {
 		this.setVidaBoss(160);
 		this.setDanoBoss(15);
 		this.setLevelBoss(4);
+	}
+	
+	public void receberDano(int danoRecebido) {
+		setVidaBoss(getVidaBoss() - danoRecebido);
 	}
 }
